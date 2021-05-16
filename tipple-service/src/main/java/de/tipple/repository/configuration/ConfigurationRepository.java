86e86f06963c49b9285678cdb10033f0fa2e0e6b
MOA <m.omarahmed@outlook.de>
2021-05-16 20:05:44 +0200

    old tipple-service removed

--- tipple-service/src/main/java/de/tipple/repository/configuration/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package de.tipple.repository.configuration;
-
-import de.tipple.model.configuration.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-@EnableReactiveMongoRepositories
-public interface ConfigurationRepository extends ReactiveCrudRepository<Configuration, String> {
-}
