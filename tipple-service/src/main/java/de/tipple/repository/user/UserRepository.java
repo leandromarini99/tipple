8c606e276564cb0f893a036e36b703440bf4755c
larsn97 <lars.newrzella97@gmail.com>
2021-05-17 17:16:20 +0200

    delete service

--- tipple-service/src/main/java/de/tipple/repository/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.tipple.repository.user;
-
-import de.tipple.model.user.User;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-@EnableReactiveMongoRepositories
-public interface UserRepository extends ReactiveCrudRepository<User, String> {
-}
