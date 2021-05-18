11d27c397b092d05897a5e0297732ad703dc9156
larsn97 <lars.newrzella97@gmail.com>
2021-05-18 09:23:39 +0200

    das

--- tipple-service/scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-./wait-for-it.sh tipple-mongo:27017 -t 30
-java -Dspring.data.mongodb.uri=mongodb://tipple-mongo:27017/tipple -Djava.security.egd=file:/dev/./urandom -jar tipple-service.jar
--- tipple-service/src/main/java/de/tipple/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package de.tipple.repository;
-
-import de.tipple.model.Ingredient;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-@EnableReactiveMongoRepositories
-public interface IngredientRepository extends ReactiveCrudRepository<Ingredient, String> {
-}
