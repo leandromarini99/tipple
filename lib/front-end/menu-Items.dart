d502c822e3edbb84f479288dbc6c8db6efb6c46a
Toni Fiebiger <fiebi99@gmail.com>
2021-05-19 20:15:32 +0200

    style home screen

--- lib/front-end/menu-Items.dart
+++ lib/front-end/menu-Items.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tipple_app/configuration/configurator-page.dart';
 import 'package:tipple_app/front-end/menu-Items-drawer.dart';
 import 'package:tipple_app/registry/app-signUp.dart';
-import 'package:tipple_app/front-end/configuration-list.dart';
 
 void main() => runApp(MyApp());
 
@@ -98,21 +99,104 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Color(0xFFFCC919),
+          iconTheme: IconThemeData(color: Color(0xFFFFFFFF)),
           title: Text(
-            "Tipple",
-            style: TextStyle(color: Colors.black),
+            "TIPPLE",
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+                color: Color(0xFFFFFFFF),
+              ),
           ),
         ),
         body: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage("assets/background.png"),
-              fit: BoxFit.fill
+              fit: BoxFit.cover
               )
             ),
-            child: Center()),
+            padding: EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 16),
+            width: double.infinity,
+            height: double.infinity,
+          child: Column(
+             children: <Widget>[
+
+               //Logo
+               Image.asset(
+                 // build(context)
+                 "assets/logo.png",
+                 width: 200,
+                 height: 200,
+               ),
+
+
+               //Greeting Text
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: RichText(
+                   text: TextSpan(
+                     text: 'Moin Username!',
+                     style: GoogleFonts.poppins(
+                       fontSize: 24,
+                       fontWeight: FontWeight.w500,
+                       color: Color(0xFF000000),
+                     ),
+                   ),
+                   textAlign: TextAlign.center,
+                 ),
+               ),
+
+
+               //Additional Text
+               Padding(
+                 padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                 child: RichText(
+                   text: TextSpan(
+                     text: 'Herzlich Willkommen bei Tipple. Mir dieser App kannst du eigene Säfte zusammenstellen.'
+                         ' Dafür stehen dir diverse natürliche Zutaten zur Auswahl,'
+                         ' welche beliebig miteinander kombiniert werden können. Du '
+                         'kannst das Getränk also ganz nach deinem eigenen Geschmack gestalten '
+                         'oder einfach experimentieren. Viel Spaß!',
+                     style: GoogleFonts.poppins(
+                       fontSize: 14,
+                       color: Color(0xFF000000),
+                     ),
+                   ),
+                   textAlign: TextAlign.justify,
+                 ),
+               ),
+
+
+               //Configurator Btn
+               Container(
+                 margin: const EdgeInsets.only(top: 50.0),
+                 width: double.infinity,
+                 child: ElevatedButton(
+                   onPressed: () {
+                     ConfiguratorPage(); //TODO
+                   },
+                   child: Text(
+                     "Zum Konfigurator",
+                     style: GoogleFonts.poppins(
+                       fontSize: 16,
+                       fontWeight: FontWeight.w700,
+                       color: Color(0xFFFFFFFF),
+                     ),
+                     textAlign: TextAlign.center,
+                   ),
+                   style: ElevatedButton.styleFrom(
+                     padding: EdgeInsets.all(17.0),
+                     primary: Color(0xFFFCC919),
+                   ),
+                 ),
+               ),
+
+             ],
+          )
+        ),
       ),
     );
   }
